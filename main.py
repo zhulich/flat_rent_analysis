c081242c5651cf3d73eff4aa5021072ab31b8e0e
Yevhenii Davydenko <akazhulich@gmail.com>
2023-02-02 04:36:17 +0200

    Restart scrapy project

--- main.py
+++ main.py
@@ -14,3 +14,28 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+import pandas as pd
+from pandas import DataFrame
+base_url = "https://www.olx.ua/d/uk/nedvizhimost/kvartiry/dolgosrochnaya-arenda-kvartir/"
+cities = ["lvov/", "kiev/"]
+currency = "?currency=UAH"
+filter_ordering = "&search%5Border%5D=created_at%3Adesc"
+filter_rooms = "&search%5Bfilter_enum_number_of_rooms_string%5D%5B0%5D="
+filter_page = "&page="
+num_of_rooms = ["odnokomnatnye", "dvuhkomnatnye", "trehkomnatnye"]
+
+url = "https://www.olx.ua/d/uk/nedvizhimost/kvartiry/dolgosrochnaya-arenda-kvartir/lvov/?currency=UAH&search%5Border%5D=created_at:desc&search%5Bfilter_enum_number_of_rooms_string%5D%5B0%5D=odnokomnatnye"
+
+response = requests.get(url)
+soup_content = BeautifulSoup(response.content, "html.parser")
+
+
+flats = soup_content.select("h6.er34gjf0")
+
+
+print(flats)
