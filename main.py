c1b9386709a6c0b93118a4374393bcd5e9812574
Yevhenii Davydenko <akazhulich@gmail.com>
2023-02-15 13:54:43 +0200

    Create main.py run spider and save data to csv

--- main.py
+++ main.py
@@ -1,41 +1,17 @@
-# This is a sample Python script.
+from scrapy.crawler import CrawlerProcess
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from olx_rent.spiders.olx_spiders import OlxSpiders
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-from bs4 import BeautifulSoup
-import requests
-import urllib.request
-import pandas as pd
-from pandas import DataFrame
-base_url = "https://www.olx.ua/d/uk/nedvizhimost/kvartiry/dolgosrochnaya-arenda-kvartir/"
-cities = ["lvov/", "kiev/"]
-currency = "?currency=UAH"
-filter_ordering = "&search%5Border%5D=created_at%3Adesc"
-filter_rooms = "&search%5Bfilter_enum_number_of_rooms_string%5D%5B0%5D="
-filter_page = "&page="
-num_of_rooms = ["odnokomnatnye", "dvuhkomnatnye", "trehkomnatnye"]
-
-url = "https://www.olx.ua/d/uk/nedvizhimost/kvartiry/dolgosrochnaya-arenda-kvartir/lvov/?currency=UAH&search%5Border%5D=created_at:desc&search%5Bfilter_enum_number_of_rooms_string%5D%5B0%5D=odnokomnatnye"
-
-response = requests.get(url)
-soup_content = BeautifulSoup(response.content, "html.parser")
+def main():
+    process = CrawlerProcess(
+        {
+            "USER_AGENT": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+        }
+    )
+    process.crawl(OlxSpiders)
+    process.start()
 
 
-flats = soup_content.select("h6.er34gjf0")
-
-
-print(flats)
+if __name__ == "__main__":
+    main()
