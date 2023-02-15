20a5f9e46e6b2275bb098077b3261a442bf372ed
Yevhenii Davydenko <akazhulich@gmail.com>
2023-02-15 13:54:03 +0200

    black refactoring

--- olx_rent/middlewares.py
+++ olx_rent/middlewares.py
@@ -53,7 +53,7 @@ class OlxRentSpiderMiddleware:
             yield r
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
 class OlxRentDownloaderMiddleware:
@@ -100,4 +100,4 @@ class OlxRentDownloaderMiddleware:
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
